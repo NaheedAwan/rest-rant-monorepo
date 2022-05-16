7271ff4e057b53dfc27c8c8f6db2d886e2f330c6
Naheed Awan <nadsawan@yahoo.com>
2022-05-16 19:15:28 -0400

    jwt

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,37 +1,51 @@
 const router = require('express').Router()
-const db = require("../models")
-const bcrypt = require('bcrypt')
-const jwt = require('jwt')
+const db = require('../models')
+const bcrypt =  require('bcrypt')
+const jwt = require('json-web-token')
 
 const { User } = db
 
 router.post('/', async (req, res) => {
-    
     let user = await User.findOne({
-        where: { email: req.body.email }
+        where: {email: req.body.email}
     })
-
-    if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
-        res.status(404).json({ 
-            message: `Could not find a user with the provided username and password` 
+    
+    if(!user || !await bcrypt.compare(req.body.password, user.passwordDigest)){
+        res.status(404).json({
+            message: `Could not find user with the provided username and password`
         })
-    } else {
-        const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId })           
-    res.json({ user: user, token: result.value })                                         
+    }else{
+        const result = await jwt.encode(process.env.JWT_SECRET, {id: user.userId})
+        res.status(200).json({ user, token: result.value })
     }
 })
 
 router.get('/profile', async (req, res) => {
+    
     try {
-        let user = await User.findOne({
-            where: {
-                userId:_                      
-            }
-        })
-        res.json(user)
+        // Split Auth header into ['Bearer', 'TOKEN']:
+        const [authenticationMethod, token] = req.headers.authorization.split(' ')
+
+        // only handle 'Bearer' auth for now
+        // we could add other auth strategies later:
+        if(authenticationMethod === 'Bearer'){
+            // decode the JWT
+            const result = await jwt.decode(process.env.JWT_SECRET, token)
+            // get the logged in user id from payload
+            const { id } = result.value
+            // find the user obj using their id:
+            let user = await User.findOne({
+                where: {
+                    userId: id
+                }
+            })
+            res.json(req.currentUser)
+
+        }
     } catch {
         res.json(null)
     }
 })
+
 
 module.exports = router
