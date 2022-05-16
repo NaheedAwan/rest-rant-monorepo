398e47f97446ffcc4cf8b23555b7616505ace831
Naheed Awan <nadsawan@yahoo.com>
2022-05-16 15:52:14 -0400

    adding jwt

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,11 +1,10 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
+const jwt = require('jwt')
 
 const { User } = db
 
-  
-  
 router.post('/', async (req, res) => {
     
     let user = await User.findOne({
@@ -17,13 +16,22 @@ router.post('/', async (req, res) => {
             message: `Could not find a user with the provided username and password` 
         })
     } else {
-        res.json({ user })
+        const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId })           
+    res.json({ user: user, token: result.value })                                         
     }
 })
 
-
-
-  
-
+router.get('/profile', async (req, res) => {
+    try {
+        let user = await User.findOne({
+            where: {
+                userId:_                      
+            }
+        })
+        res.json(user)
+    } catch {
+        res.json(null)
+    }
+})
 
 module.exports = router
